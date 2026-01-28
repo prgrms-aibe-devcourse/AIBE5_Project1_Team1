51ead8b9babbd9777a4f231249e2c94f6d539898
Mi-no-Kim <aspomn1973@gmail.com>
2026-01-28 11:04:30 +0900

    fix: PlannerPage - merge error 수정

--- src/pages/PlannerPage.tsx
+++ src/pages/PlannerPage.tsx
@@ -11,26 +11,13 @@ import { itinerary } from "../data/surveyResult"
 import KakaoMap from "../components/KakaoMap";
 
 // 관광지 카테고리, 장소
-<<<<<<< HEAD
 import { destinations } from "../data/destinations";
-=======
-import { destinationCategories, destinations } from "../data/destinations";
-
-// 식당, 호텔 장소 
-import { accommodations } from "../data/accommodations"
-import { restaurants } from "../data/restaurants"
-
-import DraggableItineraryItem from "../components/DraggableItineraryItem";
-import type { ItineraryItem }  from "../components/DraggableItineraryItem";
->>>>>>> 82809034f26fc643b253c22ddd9810ce2c8ad007
 import { restaurants } from "../data/restaurants";
 import { accommodations } from "../data/accommodations";
 
 // 식당, 호텔 장소 
 import DraggableItineraryItem from "../components/DraggableItineraryItem";
 import type { ItineraryItem }  from "../components/DraggableItineraryItem";
-
-import { itineraryArray } from "../data/itineraryArray";
 
 import { destinationCategories, accommodationCategories, restaurantCategories } from "../data/commonType";
 import { travelTypeCategories } from "../data/commonType";
@@ -60,10 +47,6 @@ export default function PlannerPage() {
     foodPrice: 0
   });
   
-  const findItineraryByKey = (planName: string) => { 
-    const plan = itineraryArray.find(item => item.key === planName);
-    return plan ? plan.value : itineraryArray[0].value;
-  }
   const updateTotalPrice = ({
     attractionPrice = 0, 
     hotelPrice = 0, 
@@ -266,13 +249,8 @@ const filteredMapItems = mapItemsFromItinerary.filter((it) => {
   return visibleDays[it.day] ?? true;
 });
 
-<<<<<<< HEAD
   const handleGoBack = (sourcePage: string) => {
     navigate(`/${sourcePage}`);
-=======
-  const handleGoBack = () => {
-    navigate("/my-plan");
->>>>>>> 82809034f26fc643b253c22ddd9810ce2c8ad007
   };
 
   const handleImageClick = (item: ItineraryItem) => {
